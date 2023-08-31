d14a48faea888dd03370df8ca17fcd5989e9510b
Victor <VictorCarmona9035@hotmail.com>
2023-08-31 08:57:56 +0200

    2- Router Module y Paginas - SPA (112)

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: '**', redirectTo: 'home' }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule,
+  ]
+})
+export class AppRoutingModule { }
